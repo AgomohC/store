fd9194aa825712a10d804a6204905c9818c9d151
AgomohC <chinaemerem98@yahoo.com>
2022-01-23 21:33:55 +0100

    added endpoint

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "https://peculiar-store-api.herokuapp.com/api";
 axios.interceptors.request.use(function (req) {
    const user = localStorage.getItem("user");
 
